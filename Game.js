35e37ebf1acb3d42e13129d4883286a3aaf2823a
Anthony1234567890l <134167391+Anthony1234567890l@users.noreply.github.com>
2024-04-21 12:56:00 -0400

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,45 +1,47 @@
-
+h = 0
 for(var i = 0; i < 5; i++){ 
-    function playRound(playerChoice, computerSelection) {
+    function playRound(playerSelection, computerSelection, h, counter) {
+        const playerSelection = 
+            playerSelection = playerSelection.toLowerCase(); 
        if(playerSelection == 'rock' && computerSelection =='scissors'){
                 console.log("You won rock beats scissors");
                 coutner = +1;
-                countergame = +1;
+                
             }
             else if (playerSelection == 'scissors' && computerSelection == 'rock'){
                 console.log("You lost rock beats scissors");
                 countner = -1;
-                countergame = +1;
+                
     
             }
             else if (playerSelection == 'scissors' && computerSelection == 'scissors'){
                 console.log("You tie");
                 countner = +0;
-                countergame = +1;
+                
     
             }
             else if (playerSelection == 'paper' && computerSelection == 'rock'){
                 console.log("You won paper beats rock");
                 countner = +1;
-                countergame = +1;
+                
     
             }
             else if (playerSelection == 'rock' && computerSelection == 'paper'){
                 console.log("You lost paper beats rock");
                 countner = -1;
-                countergame = +1;
+                
     
             }
             else if (playerSelection == 'paper' && computerSelection == 'paper'){
                 console.log("You lost rock beats scissors");
                 countner = +0;
-                countergame = +1;
+                
     
             }
             else if (playerSelection == 'rock' && computerSelection == 'rock'){
                 console.log("You tie");
                 countner = +0;
-                countergame = +1;
+                
     
             }
   
@@ -64,22 +66,18 @@ for(var i = 0; i < 5; i++){
     console.log(choice);
 }
     
-    
-    function getplayerChoice(y){
-        let y = prompt("Choose paper, rock or scissor: ");
-        const playerSelection = 
-            playerSelection = playerSelection.toLowerCase(); 
-}
+
             
     h = 5 - counter;
     return(counter, h);
+    console.log(counter, h);
 
-    const playerSelection = getplayerChoice();
+    const playerSelection = prompt("Choose paper, rock or scissor: ");
     const computerSelection = ComputerChoice();
     playRound(playerSelection, computerSelection);
 
 }
-   const playerSelection = getplayerChoice();
+   const playerSelection = prompt("Choose paper, rock or scissor: ");
     const computerSelection = ComputerChoice();
     playRound(playerSelection, computerSelection);
 
